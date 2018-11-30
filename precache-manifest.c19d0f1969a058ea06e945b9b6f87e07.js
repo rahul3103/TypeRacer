209c4b05ecbb2e660e7d9f0dbfc438f7471290ad
Rahul Shrivastava <rahul3103@gmail.com>
2018-11-30 23:30:36 +0530

    Updates

---
+++ precache-manifest.c19d0f1969a058ea06e945b9b6f87e07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dc525e025db02524764",
+    "url": "/TypeRacer/static/js/runtime~main.8dc525e0.js"
+  },
+  {
+    "revision": "c887d4cf6fa4ecc8c903",
+    "url": "/TypeRacer/static/js/main.c887d4cf.chunk.js"
+  },
+  {
+    "revision": "c637fd379abcd541ffd0",
+    "url": "/TypeRacer/static/js/1.c637fd37.chunk.js"
+  },
+  {
+    "revision": "c887d4cf6fa4ecc8c903",
+    "url": "/TypeRacer/static/css/main.12f203da.chunk.css"
+  },
+  {
+    "revision": "74b7976920b720baf11d99e7ac6cbbfd",
+    "url": "/TypeRacer/index.html"
+  }
+];
